4feb6810ea7036bea87a59e9ece3d81f15b76d18
Bereket <bereketteshome685@gmail.com>
2024-06-18 11:48:40 +0300

    configured vercel

--- server/destinyBackend/destinyBackend/settings.py
+++ server/destinyBackend/destinyBackend/settings.py
@@ -131,7 +131,9 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173"
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:5173",
+#     "http://127.0.0.1:5173"
+# ]
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
