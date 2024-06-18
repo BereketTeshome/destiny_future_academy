53a10769204259f595341bb9d5d05ab14c18aa49
Bereket <bereketteshome685@gmail.com>
2024-06-18 12:22:09 +0300

    configured vercel fixed

--- server/destinyBackend/destinyBackend/settings.py
+++ server/destinyBackend/destinyBackend/settings.py
@@ -26,10 +26,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xw*4yo+7n9ij-d2*&^&&+u@&drgdiu^-9)7^+aan+9@z#_d^zj'
+SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
 
 ALLOWED_HOSTS = ['*']
 
